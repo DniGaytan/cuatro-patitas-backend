04a1d7dae317c60b8521609f5be6743d848f3bfc
Daniel Gaytan <dnigaytan07@gmail.com>
2021-06-30 20:52:50 -0500

    fix database_local_url

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/backend-pizzablock";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/cuatro-patitas-backend";
 exports.PORT = process.env.PORT || 8080;
