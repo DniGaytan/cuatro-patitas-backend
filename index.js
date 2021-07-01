b74acdf6d9a6a1309356993e6012b684469a4d61
Daniel Gaytan <dnigaytan07@gmail.com>
2021-06-30 20:43:10 -0500

    Initial files and config

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { DATABASE_URL, PORT } = require("./config");
+
+const app = express();
+
+let server;
+function runServer(port, DATABASE_URL) {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(DATABASE_URL, (response) => {
+        if (response) {
+          return reject(response);
+        } else {
+          server = app
+            .listen(port, () => {
+              console.log("App is running on port " + port);
+              resolve();
+            })
+            .on("error", (err) => {
+              mongoose.disconnect();
+              return reject(err);
+            });
+          server.timeout = 1000000;
+        }
+      });
+    });
+  }
+  
+  function closeServer() {
+    return mongoose.disconnect().then(() => {
+      return new Promise((resolve, reject) => {
+        console.log("Closing the server");
+        server.close((err) => {
+          if (err) {
+            return reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    });
+  }
+  
+  runServer(PORT, DATABASE_URL).catch((err) => {
+    console.log(err);
+  });
+  
+  module.exports = { app, runServer, closeServer };
