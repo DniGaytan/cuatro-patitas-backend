86ecdc9002ca7ed5123b1029ce2f732ef8939ef0
Daniel Gaytan <dnigaytan07@gmail.com>
2021-07-10 19:28:50 -0500

    Modified models and routes -Created models folder -Added user model with initial architecture -Created routes folder -Added user router with initial routes

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            auto: true,
+        },
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            lowercase: true,
+            unique: true,
+            match: [/\S+@\S+\.\S+/, "is invalid"],
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        organization: {
+            type: mongoose.Schema.Types.ObjectId, ref: 'Organization',
+        }
+    }
+);
+
+const organization = mongoose.model('Organization', organizationSchema);
+
+module.exports = mongoose.model('User', userSchema);
